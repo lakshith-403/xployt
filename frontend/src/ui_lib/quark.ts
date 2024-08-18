24a576c08ce1eb453bc7bd0e50486f41a207e629
lakshith <lakshith.k.nishshanke@gmail.com>
2024-08-18 20:25:22 +0530

    add overload to quark to support directly setting html content

--- frontend/src/ui_lib/quark.ts
+++ frontend/src/ui_lib/quark.ts
@@ -12,21 +12,36 @@ export type Quark = HTMLElement
  * @param {(q: Quark) => void} [q] - An optional callback function to be executed with the new element as its argument.
  * @returns {HTMLElement} The newly created HTML element.
  */
-export function QuarkFunction(parent: Quark, tagName: string, className: string = '', attributes: Attributes = {}, q?: (q: Quark) => void): Quark {
-    const element = document.createElement(tagName)
+export function QuarkFunction(parent: Quark, tagName: string, className: string, attributes: Attributes, q?: (q: Quark) => void): Quark
 
-    element.className = className
+/**
+ * Creates a new HTML element, sets its attributes, and appends it to the parent element.
+ *
+ * @param {Quark} parent - The parent element to which the new element will be appended.
+ * @param {string} tagName - The tag name of the new HTML element (e.g., 'div', 'span').
+ * @param {string} [className=''] - The class name to be assigned to the new element. Defaults to an empty string.
+ * @param {Attributes} [attributes={}] - An object containing key-value pairs of attributes to be set on the new element. Defaults to an empty object.
+ * @param {string} [innerText] - An optional string to be set as the inner text of the new element.
+ * @returns {HTMLElement} The newly created HTML element.
+ */
+export function QuarkFunction(parent: Quark, tagName: string, className: string, attributes: Attributes, innerText?: string): Quark
+
+export function QuarkFunction(parent: Quark, tagName: string, className: string = '', attributes: Attributes = {}, qOrInnerText?: ((q: Quark) => void) | string): Quark {
+    const element = document.createElement(tagName);
+
+    element.className = className;
 
     for (const [key, value] of Object.entries(attributes)) {
-        element.setAttribute(key, value)
+        element.setAttribute(key, value);
     }
 
-    parent.appendChild(element)
+    parent.appendChild(element);
 
-    if (q != null) {
-        q(element)
+    if (typeof qOrInnerText === 'function') {
+        qOrInnerText(element);
+    } else if (typeof qOrInnerText === 'string') {
+        element.innerText = qOrInnerText;
     }
 
-
-    return element
+    return element;
 }
