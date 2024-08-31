46e5703b80427acb83493d0175e8266115edf6c7
lakshith <lakshith.k.nishshanke@gmail.com>
2024-09-01 00:28:32 +0530

    init login page

--- frontend/src/views/Login.ts
+++ frontend/src/views/Login.ts
@@ -1,20 +1,56 @@
-import {QuarkFunction as $, Quark} from '../ui_lib/quark'
-import {View, ViewHandler} from "../ui_lib/view"
+import { View, ViewHandler } from "../ui_lib/view";
+import { Quark, QuarkFunction as $ } from "../ui_lib/quark";
+import { TextField } from "../components/text_field/base";
+import { Button, ButtonType } from "../components/button/base";
 
-class LoginView implements View {
-    params: {type: string}
+export class LoginView extends View {
+  private emailField: TextField;
+  private passwordField: TextField;
+  private loginButton: Button;
 
-    constructor(params: {type: string}) {
-        this.params = params
-    }
+  constructor() {
+    super();
+    this.emailField = new TextField({
+      label: 'Email',
+      placeholder: 'Enter your email'
+    });
+    this.passwordField = new TextField({
+      label: 'Password',
+      placeholder: 'Enter your password',
+      type: 'password'
+    });
+    this.loginButton = new Button({
+      label: 'Login',
+      onClick: this.handleLogin.bind(this),
+      type: ButtonType.PRIMARY
+    });
+  }
 
-    render(q: Quark): void {
-        $(q, 'h1', '', {}, (q) => {
-            $(q, 'span', '', {}, (q) => {
-              q.innerHTML = "Login " + this.params.type
-            })
-        })
-    }
+  public render(q: Quark): void {
+    const container = $(q, 'div', 'login-container', {}, q => {
+        $(q, 'h1', 'login-title', {}, 'X PLOYT');
+        $(q, 'p', 'login-description', {}, 
+            'Log in to a straight to point description about what happens when you log in');
+
+        this.emailField.render(q);
+        this.passwordField.render(q);
+
+        $(q, 'div', 'remember-me', {}, q => {
+            $(q, 'input', '', { type: 'checkbox', id: 'rememberMe' });
+            $(q, 'label', '', { for: 'rememberMe' }, 'Remember me');
+        });
+
+        $(q, 'div', 'login-button-container', {}, q => {
+            this.loginButton.render(q);
+        });
+    });
+  }
+
+  private handleLogin(): void {
+    const email = this.emailField.getValue();
+    const password = this.passwordField.getValue();
+    console.log('Login attempt:', { email, password });
+  }
 }
 
-export const loginViewHandler = new ViewHandler('/login/{type}', LoginView)
+export const loginViewHandler = new ViewHandler('', LoginView)
