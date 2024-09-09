dd08aa114295159481537a56275ab268c821f512
Savith Panamgama <geethikasavith@gmail.com>
2024-09-10 01:56:07 +0530

    feat: add footer

--- frontend/src/components/footer/footer.ts
+++ frontend/src/components/footer/footer.ts
@@ -9,8 +9,15 @@ export class Footer {
     $(q, 'div', 'footer', {}, (q) => {
       $(q, 'div', 'footer-top', {}, (q) => {
         $(q, 'div', 'content-left', {}, (q) => {
-          $(q, 'img', 'footer-logo', { src: './../../assets/xployt-logo.png' }, '');
+          $(q, 'img', 'footer-logo', { src: './../assets/xployt-logo.png', alt: 'logo' }, '');
           $(q, 'div', 'icons', {}, (q) => {
+            new IconButton({
+              icon: 'fa-brands fa-instagram',
+              label: 'Twitter',
+              onClick: () => {
+                window.open('https://instagram.com/', '_blank');
+              },
+            }).render(q);
             new IconButton({
               icon: 'fa-brands fa-facebook',
               label: 'Twitter',
@@ -18,27 +25,47 @@ export class Footer {
                 window.open('https://twitter.com/', '_blank');
               },
             }).render(q);
-            // $(q, 'a', 'icon', { href: 'https://www.facebook.com/' }, (q) => {
-            //   $(q, 'i', 'fab fa-facebook-f', {}, '');
-            //   $(q, 'span', 'icon-text', {}, 'Facebook');
-            // });
+            new IconButton({
+              icon: 'fa-brands fa-linkedin',
+              label: 'Twitter',
+              onClick: () => {
+                window.open('https://twitter.com/', '_blank');
+              },
+            }).render(q);
+            new IconButton({
+              icon: 'fa-brands fa-discord',
+              label: 'Twitter',
+              onClick: () => {
+                window.open('https://twitter.com/', '_blank');
+              },
+            }).render(q);
           });
         });
 
-        $(q, 'div', 'content-mdle', {}, (q) => {
-          $(q, 'span', 'footer-content-right-title', {}, '© Xployt 2024. All rights reserved.');
+        $(q, 'div', 'content-middle', {}, (q) => {
+          $(q, 'span', 'link', {}, 'Validator Applications');
+          $(q, 'span', 'link', {}, 'Terms and Policy');
+          $(q, 'span', 'link', {}, 'Privacy Statement');
+          $(q, 'span', 'link', {}, 'Help Center');
+          $(q, 'span', 'link', {}, 'Sitemap');
         });
 
         $(q, 'div', 'content-right', {}, (q) => {
-          $(q, 'span', 'footer-content-right-title', {}, '© Xployt 2024. All rights reserved.');
+          $(q, 'span', 'title', {}, 'Contact Us');
+          $(q, 'div', 'contact-info', {}, (q) => {
+            $(q, 'span', 'link', {}, 'info@xployt.com');
+          });
+          $(q, 'div', 'contact-info', {}, (q) => {
+            $(q, 'span', 'link', {}, '1-800-123-4567');
+          });
+          $(q, 'div', 'contact-info', {}, 'Xployt Co, Colombo, Sri Lanka');
         });
       });
-    });
+      $(q, 'hr', 'footer-hr', {});
 
-    $(q, 'hr', 'footer-hr', {});
-
-    $(q, 'div', 'footer-bottom', {}, (q) => {
-      $(q, 'span', 'footer-content-right', {}, '© Xployt 2024. All rights reserved.');
+      $(q, 'div', 'footer-bottom', {}, (q) => {
+        $(q, 'span', 'footer-content-right', {}, '© Xployt 2024. All rights reserved.');
+      });
     });
   }
 }
