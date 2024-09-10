e3c89aaf76ebfc97c13d5ec86b611ec7366ff54c
Savith Panamgama <geethikasavith@gmail.com>
2024-09-11 02:54:30 +0530

    Refactor collapsible component styles and logic

--- frontend/src/components/table/filterable.table.ts
+++ frontend/src/components/table/filterable.table.ts
@@ -60,7 +60,7 @@ export class FilterableTable extends tableBase {
       });
     });
   }
-  private getFalseKeys(obj: { [key: string]: boolean }): string[] {
+  protected getFalseKeys(obj: { [key: string]: boolean }): string[] {
     const keys = Object.keys(obj).filter((key) => !obj[key]);
     console.log('keys', keys);
     return keys;
