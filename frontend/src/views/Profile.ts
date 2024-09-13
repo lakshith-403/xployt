f933ae113fc52be4d4daf93949e9a4d9b3da23f4
lakshith <lakshith.k.nishshanke@gmail.com>
2024-09-13 20:49:04 +0530

    Add funds section to profile

--- frontend/src/views/Profile.ts
+++ frontend/src/views/Profile.ts
@@ -9,10 +9,13 @@ import { CollapsibleBase } from "../components/Collapsible/collap.base";
 export class ProfileView extends View {
 
   private userInfoCollapsible: CollapsibleBase;
+  private fundsCollapsible: CollapsibleBase;
+
 
   constructor() {
     super();
     this.userInfoCollapsible = new CollapsibleBase("User Info", "user-info");
+    this.fundsCollapsible = new CollapsibleBase("Funds", "funds");
   }
 
   public render(q: Quark): void {
@@ -21,7 +24,7 @@ export class ProfileView extends View {
       $(q, 'div', 'profile-header', {}, (q) => {
         $(q, 'h1', '', {}, 'Hello Lakshith!');
         $(q, 'div', 'profile-picture-container', {}, (q) => {
-          $(q, 'img', 'profile-picture', { src: 'path/to/profile-picture.jpg', alt: '' });
+          $(q, 'img', 'profile-picture', { src: 'https://picsum.photos/id/237/200/300', alt: '' });
           
           $(q, 'div', 'profile-picture-button-container', {}, (q) => {
             new IconButton({
@@ -34,6 +37,7 @@ export class ProfileView extends View {
       });
 
       this.userInfoCollapsible.render(q);
+      this.fundsCollapsible.render(q);
     });
 
     $(this.userInfoCollapsible.content!, 'div', 'user-info-content', {}, (q) => {
@@ -50,6 +54,34 @@ export class ProfileView extends View {
           type: ButtonType.PRIMARY,
           onClick: () => console.log('Save changes'),
         }).render(q);
+      });
+    });
+
+    $(this.fundsCollapsible.content!, 'div', 'funds-content', {}, (q) => {
+      $(q, 'div', 'funds-details', {}, (q) => {
+        $(q, 'div', 'fund-box', {}, (q) => {
+          $(q, 'h2', 'title', {}, 'Amount Remaining');
+          $(q, 'p', 'amount', {}, '$1000');
+        });
+
+        $(q, 'div', 'fund-box', {}, (q) => {
+          $(q, 'h2', 'title', {}, 'Amount Spent');
+          $(q, 'p', 'amount', {}, '$1300');
+        });
+
+        $(q, 'div', 'fund-box button-container', {}, (q) => {
+          new Button({
+            label: 'Add Funds',
+            type: ButtonType.PRIMARY,
+            onClick: () => console.log('Add funds'),
+          }).render(q);
+
+          new Button({
+            label: 'View Transactions',
+            type: ButtonType.SECONDARY,
+            onClick: () => console.log('Withdraw funds'),
+          }).render(q);
+        });
       });
     });
   }
