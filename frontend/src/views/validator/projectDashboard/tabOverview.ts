d79bc456caff66d813d86df3473009247bdafcb4
AshJayy <ashwinie.ja@gmail.com>
2024-09-19 10:10:40 +0530

    create: Initalized ProjectOverview Component

--- frontend/src/views/validator/projectDashboard/tabOverview.ts
+++ frontend/src/views/validator/projectDashboard/tabOverview.ts
@@ -1,9 +1,32 @@
-import { Quark } from '../../../ui_lib/quark';
+import { Quark, QuarkFunction as $ } from '../../../ui_lib/quark';
+import { CACHE_STORE } from '../../../data/cache';
+import { ProjectInfoCacheMock, ProjectInfo } from '../../../data/validator/cache/projectInfo';
+import LoadingScreen from '../../../components/loadingScreen/loadingScreen';
+import  './tabOverview.scss'
 
 export default class Overview {
-  constructor(private projectId: string) {}
+  projectInfo: ProjectInfo = {} as ProjectInfo;
+  constructor(private projectId: string) {
+    this.projectId = projectId;
+  }
+  private readonly  projectInfoCache = CACHE_STORE.getProjectInfo(this.projectId) as ProjectInfoCacheMock
 
-  render(q: Quark): void {
-    q.innerHTML = 'Content for Overview';
+  async loadData(): Promise<void> {
+    try {
+      this.projectInfo = await this.projectInfoCache.get(false, this.projectId)
+      console.log('Project Info', this.projectInfo)
+    }catch (error) {
+      console.error('Failed to load project data', error);
+    }
+  }
+
+  async render(q: Quark): Promise<void> {
+    const loading = new LoadingScreen(q);
+    loading.show();
+
+    await this.loadData();
+    loading.hide()
+
+    $(q,'div', 'project-info', {}, 'Project Overview')
   }
 }
