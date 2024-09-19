3a766bfac7f58a37d210291199389cb873ddd702
Savith Panamgama <geethikasavith@gmail.com>
2024-09-19 20:56:24 +0530

    create: file input component

--- frontend/src/components/input_file/input.file.ts
+++ frontend/src/components/input_file/input.file.ts
@@ -1,22 +1,57 @@
-import { QuarkFunction as $, Quark } from '../../ui_lib/quark';
+import { QuarkFunction as $, Quark } from '@ui_lib/quark';
+import './input.file.scss';
 
-export class FileInput {
-  element: HTMLInputElement;
-  labelElement: HTMLLabelElement;
-
-  constructor(parent: HTMLElement, labelText: string) {
-    const container = $(parent, 'div', 'file-input-container', {}) as HTMLDivElement;
-
-    this.labelElement = $(container, 'label', 'file-input-label', {}) as HTMLLabelElement;
-    this.labelElement.textContent = labelText;
-
-    this.element = $(container, 'input', 'file-input', { type: 'file' }) as HTMLInputElement;
-    container.appendChild(this.element);
-  }
-
-  getFiles(): FileList | null {
-    return this.element.files;
-  }
+export interface FileInputProps {
+  label?: string;
+  className?: string;
+  accept?: string;
+  multiple?: boolean;
+  disabled?: boolean;
+  onChange?: (event: Event) => void;
+  onFocus?: (event: Event) => void;
 }
 
-export default FileInput;
+export class FileInputBase {
+  protected element?: HTMLInputElement;
+  private props: FileInputProps;
+
+  constructor(props: FileInputProps) {
+    this.props = props;
+  }
+
+  private applyProps(props: FileInputProps): void {
+    if (props.className) this.element!.classList.add(props.className);
+    if (props.accept) this.element!.accept = props.accept;
+    if (props.multiple) this.element!.multiple = props.multiple;
+    if (props.disabled) this.element!.disabled = props.disabled;
+    if (props.onChange) this.element!.addEventListener('change', props.onChange);
+    if (props.onFocus) this.element!.addEventListener('focus', props.onFocus);
+  }
+
+  public render(parent: Quark): void {
+    const container = $(parent, 'div', 'file-input-container', {});
+    if (this.props.label) {
+      $(container, 'span', 'file-input-label', {}, this.props.label);
+    }
+    this.element = $(container, 'input', 'file-input', { type: 'file' }) as HTMLInputElement;
+    this.applyProps(this.props);
+  }
+
+  public getElement(): HTMLInputElement {
+    return this.element!;
+  }
+  public setValue(value: any): void {
+    this.element!.value = value;
+  }
+  public setDisabled(disabled: boolean): void {
+    this.element!.disabled = disabled;
+  }
+
+  public setAccept(accept: string): void {
+    this.element!.accept = accept;
+  }
+
+  public setMultiple(multiple: boolean): void {
+    this.element!.multiple = multiple;
+  }
+}
