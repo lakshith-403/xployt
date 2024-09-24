a72cf1c7734da668692efa31586a7afa3941a4ea
AshJayy <ashwinie.ja@gmail.com>
2024-09-24 11:32:50 +0530

    feat: Completed project overview

--- frontend/src/views/validator/projectDashboard/tabOverview.ts
+++ frontend/src/views/validator/projectDashboard/tabOverview.ts
@@ -1,114 +1,110 @@
-import { Quark, QuarkFunction as $ } from '../../../ui_lib/quark';
-import { CACHE_STORE } from '../../../data/cache';
-import { ProjectInfoCacheMock, ProjectInfo } from '../../../data/validator/cache/projectInfo';
-import { ProjectTeamCacheMock, ProjectTeam } from '../../../data/validator/cache/project.team';
+import {Quark, QuarkFunction as $} from '../../../ui_lib/quark';
+import {CACHE_STORE} from '../../../data/cache';
+import {router} from "@ui_lib/router";
+import {ProjectInfoCacheMock, ProjectInfo} from '../../../data/validator/cache/projectInfo';
+import {ProjectTeamCacheMock, ProjectTeam} from '@data/validator/cache/project.team';
 import LoadingScreen from '../../../components/loadingScreen/loadingScreen';
-import {Card} from "../../../components/card/card.base";
-import  './tabOverview.scss'
+import {Card} from "@components/card/card.base";
+import {IconButton} from "@components/button/icon.button";
+import {OverviewPayments} from "@views/validator/projectDashboard/tabOverview/payments";
+import {OverviewReports} from "@views/validator/projectDashboard/tabOverview/reports";
+import {OverviewBasicInfo} from "@views/validator/projectDashboard/tabOverview/basicInfo";
+import './tabOverview.scss'
 
 export default class Overview {
-  projectInfo: ProjectInfo = {} as ProjectInfo
-  projectTeam: {
-    projectLead: {
-      name: string;
-      id: number;
-      username: string;
-      email: string;
-    };
-    assignedValidator: {
-      name: string;
-      id: number;
-      username: string;
-      email: string;
-    };
-  } = {
-    projectLead: {
-      name: '',
-      id: 0,
-      username: '',
-      email: ''
-    },
-    assignedValidator: {
-      name: '',
-      id: 0,
-      username: '',
-      email: ''
+    projectInfo: ProjectInfo = {} as ProjectInfo
+    projectTeam: {
+        [key: string]: {
+            name: string;
+            id: number;
+            username: string;
+            email: string;
+        }
+    } = {
+        projectLead: {
+            name: '',
+            id: 0,
+            username: '',
+            email: ''
+        },
+        assignedValidator: {
+            name: '',
+            id: 0,
+            username: '',
+            email: ''
+        }
     }
-  }
-  constructor(private readonly projectId: string) {
-    this.projectId = projectId;
-  }
-  private readonly  projectInfoCache = CACHE_STORE.getProjectInfo(this.projectId) as ProjectInfoCacheMock
-  private readonly projectTeamCache = CACHE_STORE.getProjectTeam(this.projectId) as ProjectTeamCacheMock;
 
-  async loadData(): Promise<void> {
-    try {
-      this.projectInfo = await this.projectInfoCache.get(false, this.projectId)
-      const fullTeam = await this.projectTeamCache.get(true, this.projectId);
-      this.projectTeam.projectLead = fullTeam.projectLead;
-      this.projectTeam.assignedValidator = fullTeam.validator[0]
-      console.log('Project Info', this.projectInfo)
-      console.log("Project Team", this.projectTeam)
-    }catch (error) {
-      console.error('Failed to load project data', error);
+    constructor(private readonly projectId: string) {
+        this.projectId = projectId;
     }
-  }
 
-  async render(q: Quark): Promise<void> {
-    const loading = new LoadingScreen(q);
-    loading.show();
+    private readonly projectInfoCache = CACHE_STORE.getProjectInfo(this.projectId) as ProjectInfoCacheMock
+    private readonly projectTeamCache = CACHE_STORE.getProjectTeam(this.projectId) as ProjectTeamCacheMock;
 
-    await this.loadData();
-    loading.hide()
+    async loadData(): Promise<void> {
+        try {
+            this.projectInfo = await this.projectInfoCache.get(false, this.projectId)
+            const fullTeam = await this.projectTeamCache.get(true, this.projectId);
+            this.projectTeam.projectLead = fullTeam.projectLead;
+            this.projectTeam.assignedValidator = fullTeam.validator[0]
+            console.log('Project Info', this.projectInfo)
+            console.log("Project Team", this.projectTeam)
+        } catch (error) {
+            console.error('Failed to load project data', error);
+        }
+    }
 
-    $(q,'div', 'project-info', {}, (q) => {
+    async render(q: Quark): Promise<void> {
+        const loading = new LoadingScreen(q);
+        loading.show();
 
-      $(q, 'p', '', {}, this.projectInfo.description)
+        await this.loadData();
+        loading.hide()
 
-      $(q, 'section', 'section', {id: 'basic-info'}, (q) => {
-        $(q, 'div', '', {}, (q) => {
-          $(q, 'span', '', {}, (q) => {
-            $(q, 'p', 'key', {}, 'Client')
-            $(q, 'p', 'value', {}, this.projectInfo.client)
-          })
-          $(q, 'span', '', {}, (q) => {
-            $(q, 'p', 'key', {}, "Access Link")
-            $(q, 'a', 'key link', {href: '#', target: '_blank'}, 'www.example.com')
-          })
-        })
-        $(q, 'div', '', {}, (q) => {
-          Object.entries(this.projectTeam).forEach(([key, teamMember]) => {
-            const title = convertToTitleCase(key);
+        $(q, 'div', 'project-info', {}, (q) => {
 
-            new Card({
-              title: title,
-              content: $(q, 'div', 'description', {}, (q) => {
-                $(q, 'span', '', {}, (q) => {
-                  $(q, 'p', 'value', {}, teamMember.name);
-                  $(q, 'p', 'value caption', {}, teamMember.username);
-                })
-                $(q, 'p', 'value link', {}, teamMember.email);
-              })
-            }).render(q);
-          });
-        })
-      })
+            $(q, 'p', 'project-description', {}, this.projectInfo.description)
 
-      $(q, 'section', '', {}, (q) => {
-        $(q, 'h2', '', {}, 'Rules and Scope')
-        $(q, 'ul', '', {}, (q) => {
-            this.projectInfo.scope.forEach((rule) => {
-                $(q, 'li', '', {}, rule)
+            $(q, 'div', '', {id: 'basic-info'}, (q) => {
+                new OverviewBasicInfo(this.projectId, this.projectInfo.client).render(q)
             })
+
+            $(q, 'section', '', {}, (q) => {
+                $(q, 'h2', '', {}, 'Rules and Scope')
+                $(q, 'ul', 'section-content', {}, (q) => {
+                    this.projectInfo.scope.forEach((rule) => {
+                        $(q, 'li', '', {}, rule)
+                    })
+                })
+            })
+
+            $(q, 'section', '', {id: 'payments'}, (q) => {
+                $(q, 'h2', '', {}, 'Payments')
+                new OverviewPayments(this.projectId).render(q)
+            })
+
+            $(q, 'section', '', {id: 'reports'}, (q) => {
+                $(q, 'span', '', {}, (q) => {
+                    $(q, 'h2', '', {}, 'Reports')
+                    new IconButton({
+                        icon: 'fa fa-plus',
+                        label: 'Create Report',
+                        onClick: () => {
+                            const url ='/report/' + this.projectId
+                            router.navigateTo(url)
+                        }
+                    }).render(q)
+                })
+
+
+                $(q, 'div', 'section-content', {}, (q) => {
+                    new OverviewReports({reportId: '1'}).render(q)
+                })
+            })
+
         })
-      })
-    })
-  }
+    }
 }
 
-function convertToTitleCase(input: string): string {
-  const words = input.replace(/([A-Z])/g, ' $1').trim();
-  return words.replace(/\w\S*/g, (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  });
-}
+
