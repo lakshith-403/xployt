2e805bc62c8c842e27e222257c89e222c104e1b5
Savith Panamgama <geethikasavith@gmail.com>
2024-11-28 00:20:11 +0530

    fix: userId converted to type string

--- frontend/src/data/user.ts
+++ frontend/src/data/user.ts
@@ -2,47 +2,51 @@ import { CacheObject, DataFailure } from './cacheBase';
 import { AuthEndpoints } from './network/auth.network';
 export type UserType = 'Client' | 'Validator' | 'Lead' | 'Hacker';
 
+interface UserResponse {
+  id: string;
+  username: string;
+  name: string;
+  email: string;
+  type: UserType;
+  avatar: string;
+}
 export class User {
-  id: number;
+  id: string;
   username: string;
   name: string;
   email: string;
   type: UserType;
   avatar: string;
 
-  constructor(data: any) {
+  constructor(data: UserResponse) {
     console.log('User constructor', data);
-    this.id = data['userId'];
-    this.username = data['username'];
-    this.name = data['name'];
-    this.email = data['email'];
-    this.type = data['role'];
-    this.avatar = '';
+    this.id = data.id;
+    this.username = data.username;
+    this.name = data.name;
+    this.email = data.email;
+    this.type = data.type;
+    this.avatar = data.avatar;
   }
 }
 
 export class UserCache extends CacheObject<User> {
   async load(): Promise<User> {
     const response = await AuthEndpoints.getCurrentUser();
-
-    return new User(response);
+    return new User(response.data as UserResponse);
   }
 
   async register(name: string, email: string, password: string): Promise<User> {
     const response = await AuthEndpoints.register(name, email, password);
-
-    return new User(response);
+    return new User(response.data as UserResponse);
   }
 
   async signIn(username: string, password: string): Promise<User> {
     const response = await AuthEndpoints.signIn(username, password);
-
-    return new User(response);
+    return new User(response.data as UserResponse);
   }
 
   async signOut(): Promise<void> {
     const response = await AuthEndpoints.signOut();
-
     this.invalidate_cache();
   }
 }
@@ -55,25 +59,38 @@ export class UserCache extends CacheObject<User> {
 export class UserCacheMock extends CacheObject<User> {
   async load(): Promise<User> {
     return new User({
-      id: 1,
+      id: '101',
       name: 'Mock User1',
       username: 'mock',
       email: 'mock@mock.com',
       type: process.env.ROLE as UserType,
+      avatar: '',
     });
   }
 
   async signIn(username: string, password: string): Promise<User> {
     return new User({
-      id: 1,
+      id: '102',
       name: 'Mock User2',
       username: username,
       email: 'mock@mock.com',
       type: 'Client',
+      avatar: '',
     });
   }
 
   async signOut(): Promise<void> {
     this.invalidate_cache();
+  }
+
+  async register(name: string, email: string, password: string): Promise<User> {
+    return new User({
+      id: '102',
+      name: name,
+      username: 'mock Username',
+      email: email,
+      type: 'Client',
+      avatar: '',
+    });
   }
 }
