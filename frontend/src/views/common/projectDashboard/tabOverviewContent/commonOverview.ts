f30260ea88a71a90578689e6ebe9d68c6d0f5cd1
Savith Panamgama <geethikasavith@gmail.com>
2025-04-29 10:16:04 +0530

    Refactor UIManager and project views to comment out unused error handling and project link rendering code

--- frontend/src/views/common/projectDashboard/tabOverviewContent/commonOverview.ts
+++ frontend/src/views/common/projectDashboard/tabOverviewContent/commonOverview.ts
@@ -44,7 +44,7 @@ export default class CommonOverview {
             });
 
             $(q, 'div', 'card-content', {}, (q) => {
-              $(q, 'a', 'project-link', { href: this.projectInfo.url, target: '_blank' }, this.projectInfo.url)
+              $(q, 'span', 'project-link text-light-green font-underline', {}, this.projectInfo.url);
               UIManager.listObjectGivenKeys(q, this.projectInfo, ['startDate', 'endDate', 'technicalStack', 'state'], {
                 className: 'info-list',
               });
